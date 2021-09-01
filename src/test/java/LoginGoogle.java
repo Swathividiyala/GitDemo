afcf0fc23d04401951bf92ac81bbcd2564894833
swathi <swathi.vidiyala@gmail.com>
2021-09-01 19:28:19 +0530

    First commit

---
+++ src/test/java/LoginGoogle.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginGoogle {
+
+	@Test
+	public void loginpage() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\Selenium softwares\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("javatpoint");
+		//driver.findElement(By.name("btnk")).click();
+
+
+	}
+
+}
